fe11efc6bc1765e71db0da34f0ebdf7247dcf30d
AbdullahAlroqi <71713571+AbdullahAlroqi@users.noreply.github.com>
2024-11-06 01:58:51 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -159,7 +159,7 @@ var base_url = window.location.origin;
 let model;
 (async function(){  
     console.log("model loading...");  
-    model = await tf.loadLayersModel("https://AbdullahAlroqi.github.io/handwritten-the-arabic-numbers/models/model.json")
+    model = await tf.loadLayersModel("https://abdullahalroqi.github.io/handwritten-the-arabic-numbers/models/model.json")
     console.log("model loaded..");
 })();
 
